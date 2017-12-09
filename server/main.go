040afc9a0e113145024bfd134a991c971fb9dccf
hayashitaiki <hayashitaiki@team-lab.com>
2017-12-09 23:47:17 +0900

    log吐き出し

--- server/main.go
+++ server/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"io"
 	"net/http"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	//"github.com/hidelbreq/ratel-web/server/handler"
@@ -15,6 +17,15 @@ var db *sql.DB
 
 func main() {
 	var err error
+
+	logfile, err := os.OpenFile("./logs/server.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		panic("cannnot open test.log:" + err.Error())
+	}
+	defer logfile.Close()
+	log.SetOutput(io.MultiWriter(logfile, os.Stdout))
+	log.SetFlags(log.Ldate | log.Ltime)
+
 	db, err = sql.Open("mysql", "user:pass@tcp(mysql:3306)/ratel?parseTime=true&loc=Asia%2FTokyo&charset=utf8mb4")
 	if err != nil {
 		log.Fatal(err)
